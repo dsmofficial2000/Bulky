4392253e296d6c0da52bdc1ea7a14ad0e0ffa8cf
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-17 15:43:19 +0530

    DataInsersionIntoTable

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,17 @@ namespace BulkyWeb.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        //insert data into table using Entity Framework
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(            
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
