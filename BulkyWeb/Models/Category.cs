f575ba51efc30c67e97925083b7a1bfcb81a85a9
Durgesh Marathe <dsmofficial2000@gmail.com>
2024-05-19 14:05:14 +0530

    ServerSideValidation

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,9 +8,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")] //show the name ONLY on UI
         public string Name { get; set; }
         [DisplayName("Display Order")] //show the name ONLY on UI
+        [Range(1,100,ErrorMessage ="Display order must between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
