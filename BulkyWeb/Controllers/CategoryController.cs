91c189217ba960bf5d39476213cbc0a05a618d24
Durgesh Marathe <dsmofficial2000@gmail.com>
2024-05-19 22:45:40 +0530

    Edit&UpdateCategory

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BulkyWeb.Controllers
 {
@@ -32,6 +33,43 @@ namespace BulkyWeb.Controllers
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id  == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Below 3 approaches we can use any one to retrive id from DB or find anything from DB
+            //Approach 1
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Approach 2
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id ==id);
+            //Approach 3
+            //Category categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
