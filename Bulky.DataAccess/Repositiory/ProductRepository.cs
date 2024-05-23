ef6042e9eea45df2f427ab3856b753257c7e37d1
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-23 14:58:32 +0530

    ProductRepository_andUnitOfWork

---
+++ Bulky.DataAccess/Repositiory/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repositiory.IRepositiory;
+using BulkyBook.Models;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repositiory
+{ 
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
--- Bulky.DataAccess/Repositiory/UnitOfWork.cs
+++ Bulky.DataAccess/Repositiory/UnitOfWork.cs
@@ -3,6 +3,7 @@ using BulkyBook.DataAccess.Repositiory.IRepositiory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,12 @@ namespace BulkyBook.DataAccess.Repositiory
     {
         private ApplicationDbContext _db;
         public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
         }
 
         public void Save()
