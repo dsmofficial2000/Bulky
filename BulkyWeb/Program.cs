8717b029fc7f887052e844075f7f68e1c57fdc88
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-26 16:08:13 +0530

    solve Identity Issue

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -4,7 +4,6 @@ using BulkyBook.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using BulkyBook.DataAccess.Repositiory.IRepositiory;
 using BulkyBook.DataAccess.Repositiory;
-using BulkyBookWeb.Data;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +13,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BulkyBookWebContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
