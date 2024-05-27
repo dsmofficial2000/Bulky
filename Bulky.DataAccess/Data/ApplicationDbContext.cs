025fd20cf2b653f51fc6502a74bb016ccf90b8b0
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-27 14:44:37 +0530

    Extend Identity User

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace BulkyBook.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         //insert data into table using Entity Framework
         protected override void OnModelCreating(ModelBuilder modelBuilder)
