52c646411b1053461ef396d51c2e110ff3dd7535
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-28 10:55:45 +0530

    companyidModify

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -18,9 +18,10 @@ namespace BulkyBook.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
-        public int CompanyId { get; set; }
-        [ForeignKey("CompnayId")]
+
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company Company { get; set; }        
     }
 }
