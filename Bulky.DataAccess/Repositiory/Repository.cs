3ad2b2baf960b05676a6859a598a831febdd0062
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-29 17:43:21 +0530

    Shopping Cart

--- Bulky.DataAccess/Repositiory/Repository.cs
+++ Bulky.DataAccess/Repositiory/Repository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BulkyBook.DataAccess.Repositiory
 {
@@ -48,11 +49,15 @@ namespace BulkyBook.DataAccess.Repositiory
                 }
             }
                 return query.FirstOrDefault();
-        }    
+        }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query = dbSet;            
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
             if (!string.IsNullOrEmpty(includeProperties))
             {
                 foreach (var includeProp in includeProperties
--- Bulky.Models/ShoppingCart.cs
+++ Bulky.Models/ShoppingCart.cs
@@ -24,5 +24,9 @@ namespace BulkyBook.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
+
     }
 }
