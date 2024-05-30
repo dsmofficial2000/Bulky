1f65cbf3dbd380984cdca5cb42cf84ed4747f5e1
dsmofficial2000 <dsmofficial2000@gmail.com>
2024-05-30 19:50:18 +0530

    Update Order Details

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using BulkyBook.DataAccess.Repositiory.IRepositiory;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
@@ -15,6 +16,9 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
+
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -26,14 +30,43 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
         public IActionResult Details (int orderId)
         {
-            OrderVM orderVM = new()
+            OrderVM = new()
             {
                 orderHeader = _unitOfWork.OrderHeader.Get(u=>u.Id == orderId,includeProperties: "ApplicationUser"),
                 orderDetails = _unitOfWork.OrderDetail.GetAll(u=>u.OrderHeaderId == orderId, includeProperties: "Product")
             };
-            return View(orderVM);
-        }        
-        #region API CALLS
+            return View(OrderVM);
+        }
+        
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult UpdateOrderDetail()
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.orderHeader.Id);
+            orderHeaderFromDb.Name = OrderVM.orderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = OrderVM.orderHeader.PhoneNumber;
+            orderHeaderFromDb.StreetAddress = OrderVM.orderHeader.StreetAddress;
+            orderHeaderFromDb.City = OrderVM.orderHeader.City;
+            orderHeaderFromDb.State = OrderVM.orderHeader.State;
+            orderHeaderFromDb.PostalCode = OrderVM.orderHeader.PostalCode;
+            if (!string.IsNullOrEmpty(OrderVM.orderHeader.Carrier))
+            {
+                orderHeaderFromDb.Carrier = OrderVM.orderHeader.Carrier;
+            }
+            if (!string.IsNullOrEmpty(OrderVM.orderHeader.TrackingNumber))
+            {
+                orderHeaderFromDb.Carrier = OrderVM.orderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Order Details Updated Successfully.";
+
+
+            return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
+        }
+
+            #region API CALLS
 
         [HttpGet]
         public IActionResult GetAll(string status)
